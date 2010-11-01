9700d310fe851d4a917fb50bd19f75e8d41bf694
Killabee <selim.oeksuez@bluewin.ch>
2010-11-01 20:44:41 +0100

    Projekt erstellt

---
+++ src/main/java/MavenSelim/JMockSelim/App.java
@@ -0,0 +1,13 @@
+package MavenSelim.JMockSelim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
